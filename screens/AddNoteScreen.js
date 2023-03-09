f9ded96c5fbd4dd5887633e2d74a0f9c4ec3bce2
an tran <thanhan-tran@am-bition.vn>
2023-03-09 10:53:04 +0700

    Building header

---
+++ screens/AddNoteScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const AddNoteScreen = () => {
+	return (
+		<View>
+			<Text></Text>
+		</View>
+	)
+}
+
+export default AddNoteScreen;
+
+const styles = StyleSheet.create({});
